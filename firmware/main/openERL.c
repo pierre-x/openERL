f8b9f5af313c69e8d57a2edd686977ce6126a3f6
Pierre-Xavier Rousseau <px.rousseau@aps-printers.com>
2025-02-15 18:58:22 +0100

    add firmware
    Leds are blinking

---
+++ firmware/main/openERL.c
@@ -0,0 +1,25 @@
+#include "freertos/FreeRTOS.h"
+#include "driver/gpio.h"
+
+#define YELLOW_LED_GPIO 13
+#define GREEN_LED_GPIO  14
+
+void app_main(void) {
+    gpio_config_t io_conf = {
+        .pin_bit_mask = (1ULL << YELLOW_LED_GPIO) | (1ULL << GREEN_LED_GPIO),
+        .mode = GPIO_MODE_OUTPUT_OD,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE
+    };
+    gpio_config(&io_conf);
+
+    while (1) {
+        gpio_set_level(YELLOW_LED_GPIO, 1);
+        gpio_set_level(GREEN_LED_GPIO,  0);
+        vTaskDelay(pdMS_TO_TICKS(300));
+        gpio_set_level(YELLOW_LED_GPIO, 0);
+        gpio_set_level(GREEN_LED_GPIO,  1);
+        vTaskDelay(pdMS_TO_TICKS(300));
+    }
+}
